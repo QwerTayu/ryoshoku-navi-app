c5498011e28c75b4dca48dadb06c761857cffe68
turpan9076 <10x@mbr.nifty.com>
2024-10-15 11:40:28 +0900

    ✨ #4 create table and button

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,13 +1,86 @@
 import { Liff } from "@/app/_components/Liff";
-import { Highlight } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+  Stack,
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <main>
-      <Liff />
-      <Highlight query='CHECK' styles={{ px: '1', py: '1', bg: 'orange.100' }}>
-        This is the CHECK page.
-      </Highlight>
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption></TableCaption>
+          <Thead>
+            <Tr>
+              <Th>日付</Th>
+              <Th>曜日</Th>
+              <Th>朝</Th>
+              <Th>昼</Th>
+              <Th>晩</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>10/14</Td>
+              <Td>月</Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+            <Tr>
+              <Td>10/14</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Stack spacing={2} direction='row' align='center'>
+        <Button colorScheme='teal' size='lg'>
+          Edit!
+        </Button>
+        <Button colorScheme='teal' size='lg'>
+          履歴
+        </Button>
+      </Stack>
     </main>
   );
 }
